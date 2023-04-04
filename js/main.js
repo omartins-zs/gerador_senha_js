7318a1d75424314119b4d28414489dbbf095076e
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-04 15:59:29 -0300

    Criando JavaScript e declarando as Variaveis

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const passInput = document.querySelector("#inputPasswordId");
+const lenInput = document.querySelector("#inputLengthId");
+const infoLength = document.querySelector('label[for="inputLengthId"]');
+const btnGerar = document.querySelector("#btnGerar");
+
+const chkLower = document.querySelector("#chkLowerId");
+const chkUpper = document.querySelector("#chkUpperId");
+const chkNumber = document.querySelector("#chkNumberId");
+const chkSymbols = document.querySelector("#chkSymbolsId");
