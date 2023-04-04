0e1bd2b400c93e6f9ae1c4b9ecbef16a87379f27
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-04 16:00:02 -0300

    Declarando + Variaveis e variaveis modificadas

--- js/main.js
+++ js/main.js
@@ -7,3 +7,10 @@ const chkLower = document.querySelector("#chkLowerId");
 const chkUpper = document.querySelector("#chkUpperId");
 const chkNumber = document.querySelector("#chkNumberId");
 const chkSymbols = document.querySelector("#chkSymbolsId");
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const symbols = ["!", "@", "#", "$", "%"];
+
+const caracters = Array.from(Array(26)).map((_, i) => i + 97);
+const LowercaseCaracters = caracters.map((item) => String.fromCharCode(item));
+const UppercaseCaracters = LowercaseCaracters.map((item) => item.toUpperCase());
