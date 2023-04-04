332a547a349cbdc82bfdd6e070400dc13e4daf17
omartins-zs <gabrielmatheus209@gmail.com>
2023-04-04 16:01:17 -0300

    Criando e verificando Button de Gerar senha

--- js/main.js
+++ js/main.js
@@ -14,3 +14,19 @@ const symbols = ["!", "@", "#", "$", "%"];
 const caracters = Array.from(Array(26)).map((_, i) => i + 97);
 const LowercaseCaracters = caracters.map((item) => String.fromCharCode(item));
 const UppercaseCaracters = LowercaseCaracters.map((item) => item.toUpperCase());
+
+infoLength.innerHTML = lenInput.value;
+
+lenInput.addEventListener("change", () => {
+    infoLength.innerHTML = lenInput.value;
+});
+
+btnGerar.addEventListener("click", () => {
+    generatePassword(
+        chkNumber.checked,
+        chkSymbols.checked,
+        chkLower.checked,
+        chkUpper.checked,
+        lenInput.value
+    );
+});
